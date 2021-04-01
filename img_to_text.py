1a511b114ebf05dfcf8d498ff42d958d554a783f
Bahubalendra <bahubalendra.barik@gmail.com>
2021-04-01 11:21:48 +0530

    first commit

---
+++ img_to_text.py
@@ -0,0 +1,13 @@
+#not work
+import tesseract
+import os
+from PIL import Image
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR"
+
+def convert():
+    img = Image.open('img.jpg')
+    text = pytesseract.image_to_string(img)
+    print(text)
+
+convert()
